14c9720d75e0c567d5918213f41f274736877aff
dyoshida1352 <d.yoshida1352@gmail.com>
2021-10-02 00:23:04 +0900

    ドットインストール Rubyコース完了

--- dotinstall/html.rb
+++ dotinstall/html.rb
@@ -1,20 +1,19 @@
- ##ミックスイン
+class MyError < StandardError; end #StandardErrorはRuby の標準的な例外クラス ここでは簡略的な書き方で一行にまとめている
 
-module Debug
+x = gets.to_i
 
-  def info
-    puts "#{self.class} debug info ..."
+#例外が発生しそうな処理をbeginとendで囲い、例外が発生した場合の処理を rescue の後に書く
+begin
+  if x == 3
+    raise MyError #何らかの意図があって x が 3 のときに MyError を発生させたかった場合、raiseという命令を使う
   end
-
+  p 100 / x
+rescue MyError
+  puts "not 3!"
+rescue => ex #発生した例外をexオブジェクトに入れてくれる
+  p ex.message #exオブジェクトが持っているメッセージを表示することができる
+  p ex.class
+  puts "stopped!"
+ensure #例外が発生しようがしまいが、最後に絶対実行したい処理はensureに続けて書いていくこともできる
+  puts "-- END --"
 end
-
-class Player
-  include Debug #classにミックスインするにはincludeを使用する
-end
-
-class Monster
-  include Debug
-end
-
-Player.new.info #Player debug info ...
-Monster.new.info #Monster debug info ...
