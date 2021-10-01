5ca68ae792377e3a038db3a09f60595e2e09e2c8
dyoshida1352 <d.yoshida1352@gmail.com>
2021-10-01 23:37:00 +0900

    ドットインストール#24

--- dotinstall/html.rb
+++ dotinstall/html.rb
@@ -1,12 +1,26 @@
-def sayHi(name = "tom") # 引数
-  score = 80
-  # puts "hi! #{name}"
-  return "hi! #{name}"
+def movie_encode
 end
 
-sayHi("taguchi")
-sayHi "taguchi"
-sayHi
+def movie_export
+end
 
-# p sayHi
-p score
+
+#上記について、他者が作成したメソッド名などとの衝突を考慮し、モジュールを使って自分だけの名前空間を作っていく
+
+module Movie #モジュールの一文字目は必ず大文字
+
+  VERSION = 1.1
+
+  def self.encode
+    puts "encoding..."
+  end
+
+  def self.export
+    puts "exporting..."
+  end
+
+end
+
+Movie.encode #encoding...
+Movie.export #exporting...
+p Movie::VERSION #1.1
